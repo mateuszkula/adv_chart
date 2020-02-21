ce1661e2c3b760ed8c4485e5e504cbaba1e4d631
Mateusz Kula <me@mateuszkula.com>
2020-02-21 07:48:01 +0100

    Fix styling

--- src/components/molecules/Filters/Filters.styles.js
+++ src/components/molecules/Filters/Filters.styles.js
@@ -3,6 +3,8 @@ import styled from "styled-components";
 export const Filters = styled.div`
   display: flex;
 
+  width: 100%;
+
   height: 100%;
   padding: 20px;
 
--- src/components/templates/ChartsTemplate/ChartsTemplate.styles.js
+++ src/components/templates/ChartsTemplate/ChartsTemplate.styles.js
@@ -4,6 +4,6 @@ export const Wrapper = styled.div`
   height: 100vh;
   width: 1100px;
   display: grid;
-  grid-template-columns: 3fr 7fr;
+  grid-template-columns: 30% 70%;
   grid-column-gap: 60px;
 `;
