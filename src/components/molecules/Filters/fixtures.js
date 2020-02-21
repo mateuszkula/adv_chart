31bdd92bc97c35cfa4596909dc56b6d2683a840e
Mateusz Kula <me@mateuszkula.com>
2020-02-21 07:48:10 +0100

    Add fixtures for filtering

---
+++ src/components/molecules/Filters/fixtures.js
@@ -0,0 +1,21 @@
+export const datasources = [
+  {
+    value: "datasource1",
+    label: "datasource1"
+  },
+  {
+    value: "datasource2",
+    label: "datasource2"
+  }
+];
+
+export const campaigns = [
+  {
+    value: "campaigns1",
+    label: "campaigns1"
+  },
+  {
+    value: "campaigns2",
+    label: "campaigns2"
+  }
+];
