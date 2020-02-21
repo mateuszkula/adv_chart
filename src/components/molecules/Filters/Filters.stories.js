4e071b8484550765e4675b3c1a67a268bdac242a
Mateusz Kula <me@mateuszkula.com>
2020-02-21 07:46:12 +0100

    Add data for Filters story

--- src/components/molecules/Filters/Filters.stories.js
+++ src/components/molecules/Filters/Filters.stories.js
@@ -2,5 +2,15 @@ import React from "react";
 
 import { Filters } from "./";
 import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
 
-storiesOf("molecules/Filters", module).add("default", () => <Filters />);
+import { datasources, campaigns } from "./fixtures";
+
+storiesOf("molecules/Filters", module).add("default", () => (
+  <Filters
+    campaigns={campaigns}
+    datasources={datasources}
+    onCampaignsChange={action("Campaings has changed")}
+    onDatasourcesChange={action("Datasources has changed")}
+  />
+));
